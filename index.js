e9075f8d83ea9596ef5038834b61b2617f9da150
Alvin <alvin.tol@hotmail.com>
2022-08-11 15:46:26 -0600

    adds asic configuration to server

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const bodyparser = require('body-parser')
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const app = express();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+
+app.use(cors());
+
+const uri = process.env.URL;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+mongoose.connect(uri);
+
+client.connect(err => {
+  const collection = client.db("test").collection("excerciseTracker");
+
+  if (err) return console.error(err)
+  // perform actions on the collection object
+  client.close();
 });
 
+app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyparser.urlencoded({ extended: false }))
+app.use(bodyparser.json())
 
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 
 
