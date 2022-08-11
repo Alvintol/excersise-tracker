24bd04be6e69a1a9bc6603212df48dad9c8f383e
Alvin <alvin.tol@hotmail.com>
2022-08-11 16:52:35 -0600

    adds user/exercise schemas

--- index.js
+++ index.js
@@ -6,20 +6,33 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-
 app.use(cors());
 
-const uri = process.env.URL;
+const uri = process.env.MONGO_URL;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 mongoose.connect(uri);
 
 client.connect(err => {
-  const collection = client.db("test").collection("excerciseTracker");
+  const exercises = client.db("test").collection("excerciseTracker");
+  const users = client.db("test").collection("users");
 
   if (err) return console.error(err)
   // perform actions on the collection object
   client.close();
 });
+
+const userSchema({
+  username: String
+})
+const exerciseSchema({
+  username: String,
+  description: String,
+  duration: Number,
+  date: String
+})
+
+const User = mongoose.model('User', userSchema);
+const Exercise = mongoose.model('Excerise', ExerciseSchema);
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 app.use(bodyparser.urlencoded({ extended: false }))
